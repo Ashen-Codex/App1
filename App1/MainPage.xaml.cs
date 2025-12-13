116296aa6ebf24bbc91c1537eccb618ad3061d8c
DAXFOLT <pavluhinartem@gmail.com>
2025-12-13 13:28:42 +0300

    Добавьте файлы проекта.

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a <see cref="Frame">.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialog dialog = new ContentDialog
+            {
+                Title = "Событие",
+                Content = "Кнопка была нажата!",
+                PrimaryButtonText = "ОК"
+            };
+            _ = dialog.ShowAsync();
+        }
+    }
+}
