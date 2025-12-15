c76d392cc8a1dfe4c9260203d0f408fd48333bcb
DAXFOLT <pavluhinartem@gmail.com>
2025-12-15 18:54:21 +0300

    f

---
+++ App1/CustomControl1.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace App1
+{
+    public sealed class CustomControl1 : Control
+    {
+        public CustomControl1()
+        {
+            // Ключ для поиска стиля по умолчанию в Generic.xaml
+            this.DefaultStyleKey = typeof(CustomControl1);
+        }
+    }
+}
